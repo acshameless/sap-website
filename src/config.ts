769df710a877ae80f8df99e01edc48624900a0f5
acshameless <acshameless@gmail.com>
2024-11-23 01:50:39 +0800

    feat: init repo

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export const SITE = {
+  title: 'SAP Notes',
+  author: 'shameless',
+  description: '记录工程师 shameless 的 AWS SAP 刷题记录，不定期发布，欢迎关注',
+  keywords: 'ac,shameless,Blog,AWS,SAP',
+  icon: 'https://aea62e6.webp.li/2024/11/favicon.png',
+  pic: 'https://aea62e6.webp.li/2024/11/favicon.png',
+  homePage: 'https://sap.shameless.top',
+  blogPage: 'https://ac.shameless.top/',
+  twitterId: 'acshameless',
+  githubId: 'acshameless',
+  repo: 'acshameless/sap-website',
+};
